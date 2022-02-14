1ce88d6f7e843271d089618e3be9cff7e11e7ce0
Jeffrey Jones <linkjjones@gmail.com>
2022-02-14 11:51:33 -0500

    adds drag and drop functionality

--- kanban-fire/src/app/app.component.ts
+++ kanban-fire/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './task/task';
+import { transferArrayItem, CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { Task } from './task/task';
 })
 export class AppComponent {
   title = 'kanban-fire';
+  // swim lanes
   todo: Task[] = [
     {
       title: 'go to Costco',
@@ -18,4 +20,20 @@ export class AppComponent {
       description: 'Costo gas is CHEAP!'
     }
   ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void { }
+  drop(event: CdkDragDrop<Task[] | any>): void {
+    // if the task is in the same container, don't do anything
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
